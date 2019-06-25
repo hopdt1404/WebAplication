0d88047654a831d9a0530afaf886d0b2e4dbef30
hopdt <thjenhop99@gmail.com>
2019-06-25 17:48:30 +0700

    Create Project Automatic Test CShape

---
+++ AutomationTestCShape/AutomationTestCShape/Models/Blog.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutomationTestCShape.Models
+{
+    public class Blog
+    {
+        private string _url = "http://blog.testproject.io/";
+        private ChromeDriver _driver;
+        public Blog()
+        {
+            _driver = new ChromeDriver();
+        }
+        public Blog(string url)
+        {
+            _url = url;
+            _driver = new ChromeDriver();
+        }
+    }
+}
