5aca4fc112243a1f11fac5d0ced9fb889a20383d
hopdt <thjenhop99@gmail.com>
2019-06-26 15:18:50 +0700

    NotDone Access Modifier

---
+++ AccessModifierCShape/AccessModifierCShape/PracticeAccessModifier/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AccessModifierCShape.PracticeAccessModifier
+{
+    public class User
+    {
+        private string _UserName {
+            set { _UserName = value;}
+            get { return _UserName; }
+        }
+        string ITem { set; get; }
+
+        public User(string userName)
+        {
+            _UserName = userName;
+        }
+
+
+        
+    }
+}
