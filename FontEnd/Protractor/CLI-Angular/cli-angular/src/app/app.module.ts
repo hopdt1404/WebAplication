4fbc0e21e941d398b97964ae7dda621fd24d377b
confino <thjenhop99>
2019-07-10 10:19:53 +0700

    none

--- FontEnd/Protractor/CLI-Angular/cli-angular/src/app/app.module.ts
+++ FontEnd/Protractor/CLI-Angular/cli-angular/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HomePageComponent } from './home/home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
